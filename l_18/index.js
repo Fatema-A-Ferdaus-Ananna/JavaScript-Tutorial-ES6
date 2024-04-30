c21ccc3f8572af9c226a860ab0bb0d063cf117ce
Fatema-A-Ferdaus <fatema.a.ferdaus@gmail.com>
2024-04-30 07:05:28 +0600

    array methods

--- l_18/index.js
+++ l_18/index.js
@@ -1,12 +1,20 @@
 //startsWith(searchingString, position) -> check a string starts with another string
 
 const message = "Today is 30 April, Tuesday";
-document.write(message.startsWith('Today'));
+document.write(`Message = ${message} <br>`);
+document.write(`The sentance starts with string 'Today' : ${message.startsWith('Today')}<br><br>`);
+document.write(`The sentance at index 6 starts with string 'Today' : 
+                ${message.startsWith('Today', 6)} <br><br>`);
 
 //endWith(searching, length) --> check a string ends with another string
 
+document.write(`The sentance ends with string 'Today' : ${message.endsWith('Today')}<br>`);
+document.write(`The sentance ends with string 'Tuesday' : ${message.endsWith('Tuesday')}<br><br>`);
 
-//includes(searching, position) --> check if a string contains anoyher string
+
+//includes(searching, position) --> check if a string contains another string
+document.write(`The sentance contains string 'today' : ${message.includes('today')}<br>`); //case sensitie
+document.write(`The sentance ends with string 'Today' : ${message.includes('Today')}<br>`);
 
 
 //all these methods are case sensitive
