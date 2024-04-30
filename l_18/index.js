582ad61ad263c0a57ae92165bd4da4a1ecfced44
Fatema-A-Ferdaus-Ananna <106979216+Fatema-A-Ferdaus-Ananna@users.noreply.github.com>
2024-04-30 06:47:08 +0600

    l_18

---
+++ l_18/index.js
@@ -0,0 +1,13 @@
+//startsWith(searchingString, position) -> check a string starts with another string
+
+const message = "Today is 30 April, Tuesday";
+document.write(message.startsWith('Today'));
+
+//endWith(searching, length) --> check a string ends with another string
+
+
+//includes(searching, position) --> check if a string contains anoyher string
+
+
+//all these methods are case sensitive
+
